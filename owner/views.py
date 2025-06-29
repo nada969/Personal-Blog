b2541551aacd1ecc7554d8b0b8fed3881313ce57
nada969 <nadamousa969@gmail.com>
2025-06-29 23:40:46 +0300

    add file model

--- owner/views.py
+++ owner/views.py
@@ -1,5 +1,24 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from .forms import UploadFileForm
 
-# Create your views here.
+
 def index(request):
     return render(request,"owner/base.html")
+
+def handle_uploaded_file(f):
+    with open("some/file/name.txt", "wb+") as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+
+def upload_file(request):
+    if request.method == "POST":
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            handle_uploaded_file(request.FILES["file"])
+            return HttpResponseRedirect("owner/dashboard.html")
+    else:
+        form = UploadFileForm()
+    return render(request, "owner/upload.html", {"form": form})
+
+
